42c9564cf7ef2702ba35d35f614836fd7b093fde
Richard Anthony Fernandes <55104357+rfernand387@users.noreply.github.com>
2022-03-04 20:11:33 -0500

    Updated for L08 collection 2

--- Source-js/toolsL08.js
+++ Source-js/toolsL08.js
@@ -1,4 +1,3 @@
-
 // landsat 8  land mask
 // clear and no water, cloud shadow, snow, water
 var L08MaskLand = function(image) {
@@ -6,7 +5,6 @@ var L08MaskLand = function(image) {
 
   var qa = image.select('QA_PIXEL');
   var mask = qa.bitwiseAnd(1).eq(0)
-                  .and(qa.bitwiseAnd(1<<6).eq(1))
                   .and(qa.bitwiseAnd(1<<4).eq(0))
                   .and(qa.bitwiseAnd(1<<5).eq(0))
                   .and(qa.bitwiseAnd(1<<7).eq(0))
@@ -29,17 +27,21 @@ var L08MaskClear = function(image) {
 
 
 
-// add L8 geomtery bands
+
+
+
+// add L8 geomtery bands cosine
 var addL08Geometry = function(colOptions,image) {
-  colOptions = ee.Dictionary(colOptions)
-  image = ee.Image(image)
   
-  var sza = ee.Image.constant(180).subtract(ee.Image(image.metadata(colOptions.get("sza"))));
-  
-  return image.addBands(ee.Image.constant(0).multiply(3.1415).divide(180).cos().rename(['cosVZA']))
-              .addBands(sza .multiply(3.1415).divide(180).cos().rename(['cosSZA']))
-              .addBands(image.metadata(colOptions.get("saa")).subtract(image.metadata(colOptions.get("saa"))).multiply(3.1415).divide(180).cos().rename(['cosRAA']))
-}
+  var geometryBands = ee.ImageCollection("LANDSAT/LC08/C02/T1_TOA").filterMetadata('system:index','equals',image.get('system:index')) 
+                                                                      .first() 
+
+  return image.addBands(geometryBands.select([colOptions.vza,colOptions.sza]).divide(1000).multiply(3.1415).divide(180).cos().multiply(1000).toUint16().rename('cosVZA','cosSZA'))
+              .addBands(geometryBands.select(colOptions.vaa).subtract(geometryBands.select(colOptions.saa)).divide(1000).multiply(3.1415).divide(180).cos().multiply(1000).toUint16().rename('cosRAA'))
+}      
+
+
+
 exports.L08MaskClear = L08MaskClear;
 exports.L08MaskLand = L08MaskLand;
 exports.addL08Geometry = addL08Geometry;
